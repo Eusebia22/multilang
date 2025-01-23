cdad49963a276d8cbb73206041b3ead0ce68399b
EuseIlai <esubiaphilomina@gmail.com>
2025-01-23 21:47:52 +0100

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,9 +1,18 @@
-import { languageSelect, fetchTranslations } from "./script.js";
+// import { languageSelect, fetchTranslations } from "./script.js";
 
-// Load default language
-fetchTranslations("en", "index");
+// // Load default language
+// fetchTranslations("en", "index");
+
+// // Change language on selection
+// languageSelect.addEventListener("change", (event) => {
+// 	fetchTranslations(event.target.value, "index");
+// });
+import { languageSelect, initializeLanguage, updateLanguage } from "./script.js";
+
+// Initialize language for the contact page
+initializeLanguage("contact");
 
 // Change language on selection
 languageSelect.addEventListener("change", (event) => {
-	fetchTranslations(event.target.value, "index");
+    updateLanguage(event.target.value, "contact");
 });
