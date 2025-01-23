ae25170768efd09e5e3b3928b092fffc277bd427
EuseIlai <esubiaphilomina@gmail.com>
2025-01-23 21:49:22 +0100

    Update contact.js

--- public/contact.js
+++ public/contact.js
@@ -1,9 +1,19 @@
-import { languageSelect, fetchTranslations } from "./script.js";
+// import { languageSelect, fetchTranslations } from "./script.js";
 
-// Load default language
-fetchTranslations("en", "contact");
+// // Load default language
+// fetchTranslations("en", "contact");
+
+// // Change language on selection
+// languageSelect.addEventListener("change", (event) => {
+// 	fetchTranslations(event.target.value, "contact");
+// });
+
+import { languageSelect, initializeLanguage, updateLanguage } from "./script.js";
+
+// Initialize language for the contact page
+initializeLanguage("contact");
 
 // Change language on selection
 languageSelect.addEventListener("change", (event) => {
-	fetchTranslations(event.target.value, "contact");
+    updateLanguage(event.target.value, "contact");
 });
